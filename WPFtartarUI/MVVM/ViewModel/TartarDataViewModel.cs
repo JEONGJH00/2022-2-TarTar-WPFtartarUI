30be9375a8fb6770c40223c5fb20a91a8c6d0d85
이선희 <@LAPTOP-ADMQ6Q87>
2022-12-08 14:37:56 +0900

    초기 커밋.

---
+++ WPFtartarUI/MVVM/ViewModel/TartarDataViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using WPFtartarUI.Core;
+
+namespace WPFtartarUI.MVVM.ViewModel
+{
+    class TartarDataViewModel : ObservableObject
+    {
+        
+        public RelayCommand RecentTartarDataCommand { get; set; }
+
+        public RecentTartarDataViewModel RecentTartarDataVM { get; set; }
+        
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public TartarDataViewModel()
+        {
+            RecentTartarDataVM = new RecentTartarDataViewModel();
+            
+
+            CurrentView = RecentTartarDataVM;
+
+            RecentTartarDataCommand = new RelayCommand(o =>
+            {
+                CurrentView = RecentTartarDataVM;
+            });
+
+        }
+    }
+}
